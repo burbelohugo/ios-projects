d4c66ff847f13e3c52bd9b8082d4481701ad734f
Hugo Burbelo <hburbelo@phone2action.com>
2020-03-18 14:49:37 -0400

    Fix looping bug

--- Quizzler/Quizzler-iOS13/ViewController.swift
+++ Quizzler/Quizzler-iOS13/ViewController.swift
@@ -9,10 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["Four + two is equal to 6", "True"],
+        ["Five - three is greater than 1", "True"],
+        ["Three + eight is less than 10", "True"]
+    ]
+    
+    var questionNumber = 0
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle // True, False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if questionNumber < quiz.count - 1{
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
     }
 
 
