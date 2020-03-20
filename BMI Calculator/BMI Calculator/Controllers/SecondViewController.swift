7df5f9b486905b7cc0d865519ce0a2bf8caaffc3
Hugo Burbelo <hburbelo@phone2action.com>
2020-03-20 14:00:11 -0400

    Add SecondViewController

---
+++ BMI Calculator/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Hugo Burbelo on 3/20/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
