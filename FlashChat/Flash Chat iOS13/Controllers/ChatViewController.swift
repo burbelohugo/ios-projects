72571c193c26511a66ff2b5c651ba7b60efb16bb
Hugo Burbelo <hburbelo@phone2action.com>
2020-03-31 13:42:36 -0400

    Customize cells in tableview with .xib file

--- FlashChat/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ FlashChat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,9 +14,18 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "burbelo@gmail.com", body: "Hey!"),
+        Message(sender: "burbelo@gmail.com", body: "Hey!"),
+        Message(sender: "burbelo@gmail.com", body: "Hey!"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -31,4 +40,16 @@ class ChatViewController: UIViewController {
         }
     }
     
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
 }
